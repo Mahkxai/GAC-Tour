c9c06c64a8a4ebdb31b888889371e5ec7cf83b1b
Mahkxai <shr.hardik@gmail.com>
2024-02-24 12:09:16 -0600

    Completed: Splashscreen + Logo. Animated SS for Android 12+. Static for rest.

--- androidApp/src/main/java/com/mahkxai/gactour/android/presentation/MainActivity.kt
+++ androidApp/src/main/java/com/mahkxai/gactour/android/presentation/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material3.*
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.mahkxai.gactour.android.presentation.theme.GACTourTheme
 import com.mapbox.maps.MapboxExperimental
@@ -19,6 +20,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            // Checks while the splash screen is loading
+        }
         setContent {
             GACTourTheme {
                 GACTourApp()
