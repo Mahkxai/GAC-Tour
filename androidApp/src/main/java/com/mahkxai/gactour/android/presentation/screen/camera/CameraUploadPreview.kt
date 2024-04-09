6e3d6c94149dbf7348bf660c9d85c017855b0fc8
Hardik Shrestha <shr.hardik@gmail.com>
2024-04-09 03:21:58 -0500

    Completed: SnapChat like draggable caption. WIP: Integrate caption for media upload

--- androidApp/src/main/java/com/mahkxai/gactour/android/presentation/screen/camera/CameraUploadPreview.kt
+++ androidApp/src/main/java/com/mahkxai/gactour/android/presentation/screen/camera/CameraUploadPreview.kt
@@ -5,15 +5,21 @@ import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,13 +29,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -39,6 +50,7 @@ import com.mahkxai.gactour.android.domain.model.GACTourUploadItem
 import com.mahkxai.gactour.android.presentation.theme.RichGold
 import com.mapbox.geojson.Point
 import kotlinx.coroutines.delay
+import kotlin.math.roundToInt
 
 @Composable
 fun CameraUploadPreview(
@@ -63,7 +75,7 @@ fun CameraUploadPreview(
         floatingActionButton = {
             if (!isUploading) {
                 UploadButton {
-                    currentLocation?.let {  location ->
+                    currentLocation?.let { location ->
                         onUpload(
                             GACTourMediaType.IMAGE,
                             GACTourUploadItem(
@@ -87,7 +99,9 @@ fun CameraUploadPreview(
         ) {
             when (mediaType) {
                 GACTourMediaType.IMAGE -> ImageUploadPreview(mediaUri = mediaUri)
-                GACTourMediaType.VIDEO -> { /* TODO: Upload video */ }
+                GACTourMediaType.VIDEO -> { /* TODO: Upload video */
+                }
+
                 else -> throw IllegalArgumentException("Unsupported media type: $mediaType")
             }
 
@@ -106,17 +120,81 @@ fun CameraUploadPreview(
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ImageUploadPreview(
     mediaUri: Uri
 ) {
-    AsyncImage(
-        model = mediaUri,
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    var caption by remember { mutableStateOf("") }
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
+            .background(Color.Black)
+    ) {
+        AsyncImage(
+            model = mediaUri,
+            modifier = Modifier
+                .fillMaxSize(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+
+        DraggableCaption(
+            caption = caption,
+            offsetY = offsetY,
+            captionMaxChars = 80,
+            captionMaxLines = 5,
+            onTextDragged = { offsetY += it },
+        ) { caption = it }
+    }
+}
+
+@Composable
+fun BoxScope.DraggableCaption(
+    caption: String,
+    offsetY: Float,
+    captionMaxChars: Int,
+    captionMaxLines: Int,
+    onTextDragged: (Float) -> Unit,
+    onTextChange: (String) -> Unit
+) {
+    BasicTextField(
+        value = caption,
+        onValueChange = {
+            if (it.length <= captionMaxChars && it.count { it == '\n' } <= captionMaxLines) {
+                onTextChange(it)
+            }
+        },
+        modifier = Modifier
+            .offset { IntOffset(0, offsetY.roundToInt()) }
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    onTextDragged(dragAmount.y)
+                }
+            }
+            .background(Color.Black.copy(alpha = 0.8f))
+            .fillMaxWidth()
+            .align(Alignment.Center)
+            .padding(4.dp),
+        textStyle = TextStyle(
+            fontSize = 20.sp,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        ),
+        decorationBox = { innerTextField ->
+            if (caption.isEmpty()) {
+                Text(
+                    text = "Enter caption...",
+                    color = Color.Gray,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+            innerTextField()
+        },
     )
 }
 
@@ -192,7 +270,7 @@ fun UploadOverlay(
         )
 
         CircularProgressIndicator(
-            progress = progress.toFloat()/100,
+            progress = progress.toFloat() / 100,
         )
     }
 }
